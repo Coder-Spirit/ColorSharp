a3ecc2dfda90a0ca7e4729b66a6b6d05068829d4
Andreu Correa Casablanca <castarco@gmail.com>
2014-12-10 14:13:25 +0100

    Partial work, unstable, unsafe

--- ColorSharp/build_nuget_pkg.sh
+++ ColorSharp/build_nuget_pkg.sh
@@ -1,1 +1,5 @@
-nuget pack ColorSharp.csproj -build -Prop Platform=AnyCPU -Prop Configuration=Release -IncludeReferencedProjects
+# msbuild ColorSharp.csproj /t:Build /p:Configuration="Release 4.0"
+#nuget pack ColorSharp.csproj -build -Prop Platform=AnyCPU -Prop Configuration="Release 4.5" -IncludeReferencedProjects
+
+xbuild /target:Build /property:Configuration="Release 4.5"  ColorSharp.csproj
+xbuild /target:Build;Package; /property:Configuration="Release 4.0" ColorSharp.csproj
